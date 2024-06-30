1cc3666edc0ffe4bf6476f22932dd09fccbc17c8
jonik2909 <boronovjonibek@gmail.com>
2024-06-30 17:26:11 +0900

    feat: build widgets and create todo logic development

--- lib/screens/add_page.dart
+++ lib/screens/add_page.dart
@@ -1,4 +1,7 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
 class AddTodoPage extends StatefulWidget {
   const AddTodoPage({super.key});
@@ -8,6 +11,51 @@ class AddTodoPage extends StatefulWidget {
 }
 
 class _AddTodoPageState extends State<AddTodoPage> {
+  TextEditingController titleController = TextEditingController();
+  TextEditingController descriptionController = TextEditingController();
+
+  Future<void> submitData() async {
+    // get the data from form
+    final title = titleController.text;
+    final description = descriptionController.text;
+    final body = {
+      "title": title,
+      "description": description,
+      "is_completed": false
+    };
+    // submit data to the server
+    final url = 'https://api.nstack.in/v1/todos';
+    final uri = Uri.parse(url);
+    final response = await http.post(uri,
+        body: jsonEncode(body), headers: {'Content-Type': 'application/json'});
+
+    if (response.statusCode == 201) {
+      showSuccessMessage('success', 'Creation Success!');
+      Navigator.pop(context);
+    } else {
+      showSuccessMessage('fail', 'Creation failed!');
+    }
+    print(response.body);
+    // show success or fail message based on status
+  }
+
+  void showSuccessMessage(String type, String message) {
+    var snackBar;
+    if (type == 'success') {
+      snackBar = SnackBar(content: Text(message));
+    } else {
+      snackBar = SnackBar(
+        content: Text(
+          message,
+          style: TextStyle(color: Colors.white),
+        ),
+        backgroundColor: Colors.red,
+      );
+    }
+
+    ScaffoldMessenger.of(context).showSnackBar(snackBar);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -18,9 +66,11 @@ class _AddTodoPageState extends State<AddTodoPage> {
         padding: EdgeInsets.all(20),
         children: [
           TextField(
+            controller: titleController,
             decoration: InputDecoration(hintText: 'Title'),
           ),
           TextField(
+            controller: descriptionController,
             decoration: InputDecoration(hintText: 'Description'),
             keyboardType: TextInputType.multiline,
             minLines: 5,
@@ -30,7 +80,7 @@ class _AddTodoPageState extends State<AddTodoPage> {
             height: 20,
           ),
           ElevatedButton(
-            onPressed: () {},
+            onPressed: submitData,
             child: Text('Submit'),
           )
         ],
