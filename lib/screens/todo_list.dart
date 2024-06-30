202bd295e3f38fd689b4a658874efc385ffba9f1
jonik2909 <boronovjonibek@gmail.com>
2024-06-30 17:36:06 +0900

    feat: get all todo data and render as widget

--- lib/screens/todo_list.dart
+++ lib/screens/todo_list.dart
@@ -1,5 +1,8 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:todo/screens/add_page.dart';
+import 'package:http/http.dart' as http;
 
 class TodoListPage extends StatefulWidget {
   const TodoListPage({super.key});
@@ -9,6 +12,35 @@ class TodoListPage extends StatefulWidget {
 }
 
 class _TodoListPageState extends State<TodoListPage> {
+  List items = [];
+
+  void navigateToAddPage() {
+    final route = MaterialPageRoute(builder: (context) => AddTodoPage());
+    Navigator.push(context, route);
+  }
+
+  Future<void> fetchTodo() async {
+    final url = 'https://api.nstack.in/v1/todos?page=1&limit=10';
+    final uri = Uri.parse(url);
+    final response = await http.get(uri);
+
+    if (response.statusCode == 200) {
+      final json = jsonDecode(response.body) as Map;
+      final result = json['items'] as List;
+      setState(() {
+        items = result;
+      });
+    } else {}
+
+    print(response.body);
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    fetchTodo();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -16,15 +48,22 @@ class _TodoListPageState extends State<TodoListPage> {
         // backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: Text('Todo List'),
       ),
+      body: ListView.builder(
+          itemCount: items.length,
+          itemBuilder: (context, index) {
+            final item = items[index] as Map;
+            return ListTile(
+              leading: CircleAvatar(
+                child: Text('${index + 1}'),
+              ),
+              title: Text(item['title']),
+              subtitle: Text(item['description']),
+            );
+          }),
       floatingActionButton: FloatingActionButton.extended(
         onPressed: navigateToAddPage,
         label: Text("Add Todo"),
       ),
     );
-  }
-
-  void navigateToAddPage() {
-    final route = MaterialPageRoute(builder: (context) => AddTodoPage());
-    Navigator.push(context, route);
   }
 }
